0246730c1ab6443fb4fb157019a461925414ef57
Matheus Soares <matheusmontenegro2011@hotmail.com>
2023-07-17 18:59:03 -0300

    Adicionando a medição de tempo e os comentários

--- main/SA.py
+++ main/SA.py
@@ -2,77 +2,85 @@
 
 import math
 import random
+import time
 import importlib
 from FFD import Le_Instancia 
 from FFD import FirstFitDecreasing
 
-# Objective function using the FirstFitDecreasing heuristic
+# Gerando a solução inicial usando a heurística FirstFitDecreasing
 def objective_function(weight, n, c):
     return FirstFitDecreasing(weight, n, c)
 
-# Technique 1: Relocation of a single selected item from one bin to another randomly selected bin
+# Técnica 1: Realocação de um único item selecionado de um bin para outro bin selecionado aleatoriamente
 def technique1(weight, n, c):
-    # Randomly select an item and its current bin
+    # Aleatoriamente seleciona um item e seu bin atual
     item_idx = random.randint(0, n - 1)
     current_bin = weight[item_idx]
     
-    # Randomly select a new bin
+    # Aleatoriamente seleciona um novo bin
     new_bin = random.randint(0, n - 1)
     
-    # Relocate the item to the new bin
+    # Realoca o item para o novo bin
     weight[item_idx] = new_bin
     
     return weight
 
-# Technique 2: Random selection of two items allocated in two different bins and exchanging their positions
+# Técnica 2: Seleção aleatória de dois itens alocados em dois bins diferentes e troca de suas posições
 def technique2(weight, n, c):
-    # Randomly select two items allocated in different bins
+    # Aleatoriamente seleciona dois itens alocados em bins diferentes
     item_idx1, item_idx2 = random.sample(range(n), 2)
     
-    # Exchange the positions of the two items
+    # Troca as posições dos dois itens
     weight[item_idx1], weight[item_idx2] = weight[item_idx2], weight[item_idx1]
     
     return weight
 
-# Simulated Annealing implementation
 def simulated_annealing(weight, n, c, initial_temperature, cooling_rate, stopping_temperature):
-    # Initial solution
+    # Inicializa a solução atual com a função objetivo
     current_solution = objective_function(weight, n, c)
     best_solution = current_solution
 
-    # Initialize temperature
+    # Inicializa a temperatura
     temperature = initial_temperature
 
     # Simulated Annealing loop
     while temperature > stopping_temperature:
-        # Apply Technique 1 at higher temperatures, and Technique 2 at lower temperatures
+        # Aplica a técnica 1 quando a temperatura é maior que a metade da temperatura inicial e a técnica 2 caso contrário
         if temperature > (initial_temperature / 2):
             new_weight = technique1(list(weight), n, c)
         else:
             new_weight = technique2(list(weight), n, c)
 
-        # Calculate the objective function for the new solution
+        # Calcula a função objetivo da nova solução
         new_solution = objective_function(new_weight, n, c)
 
-        # Accept the new solution if it improves the objective function
+        # Aceita a nova solução se ela for melhor que a solução atual
         if new_solution < current_solution:
             weight = new_weight
             current_solution = new_solution
         else:
-            # Accept the new solution probabilistically based on the temperature
+            # Aceita a nova solução probabilisticamente baseado na temperatura
             probability = math.exp((current_solution - new_solution) / temperature)
             if random.random() < probability:
                 weight = new_weight
                 current_solution = new_solution
 
-        # Update the best solution
+        # Atualiza a melhor solução
         if current_solution < best_solution:
             best_solution = current_solution
 
-        # Decrease the temperature
+        # Diminui a temperatura
         temperature *= cooling_rate
 
     return best_solution
+
+# Mede o tempo de execução do Simulated Annealing
+def measure_execution_time(weight, n, c, initial_temperature, cooling_rate, stopping_temperature):
+    start_time = time.time()
+    best_solution = simulated_annealing(weight, n, c, initial_temperature, cooling_rate, stopping_temperature)
+    end_time = time.time()
+    execution_time = end_time - start_time
+    return best_solution, execution_time
 
 # Testa uma única instancia usando Simulated Annealing
 filename = "/home/TEO/main/Wäscher/Waescher_TEST0005.txt"
@@ -86,7 +94,9 @@ if weight:
     cooling_rate = 0.95
     stopping_temperature = 0.1
 
-    best_solution = simulated_annealing(weight, n, c, initial_temperature, cooling_rate, stopping_temperature)
+    best_solution, execution_time = measure_execution_time(weight, n, c, initial_temperature, cooling_rate, stopping_temperature)
     print("Best solution found using Simulated Annealing for", filename, ":", best_solution)
+    print("Execution time:", execution_time, "seconds")
+
 
 
